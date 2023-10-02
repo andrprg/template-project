939cc5be1c2f7785cf8dda3e394607f6a4f3e4b5
DESKTOP-TREDQN8\Andrey <andrprg@yandex.ru>
2023-10-02 16:08:56 +0300

    ui component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './ui/theme/layout/components/header/header.component';
-import { FooterComponent } from './ui/theme/layout/components/footer/footer.component';
-import { MainComponent } from './ui/theme/layout/components/main/main.component';
+import { HeaderComponent } from './ui/layout/components/header/header.component';
+import { FooterComponent } from './ui/layout/components/footer/footer.component';
+import { MainComponent } from './ui/layout/components/main/main.component';
 
 @NgModule({
   declarations: [
